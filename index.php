f01d5e4635b481c5a1c16268abc72be23985c916
georgeinaneishvili <george.inaneishvili@gmail.com>
2022-06-06 11:24:31 +0400

    recode everything

--- index.php
+++ index.php
@@ -8,11 +8,45 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Marvel Comics API Search - character</title>
 
-  <link rel="stylesheet" href="style.css">
+
+
+  <style>
+    body{
+      background-color: #000000;
+    }
+
+
+    .searchtext{
+    width: 80%;
+    background-color: #8492A6;
+    margin-left: 10%;
+    margin-right: 10%;
+    height: 30px;
+    margin-top: 40px;
+    text-align: center;
+    color: white;
+}
+
+.searhbutton{
+    width: 40%;
+    background-color: #F9FAFC;
+    margin-left: 30%;
+    margin-right: 30%;
+    height: 30px;
+    margin-top: 40px;
+}
+
+
+
+  </style>
 
 </head>
 <body>
-
+  
+<form>
+<input type="text" class="searchtext" placeholder="hero name"> </input>
+<input type="button" class="searhbutton" name="submit" value="submit">
+</form>
 
 </body>
 
